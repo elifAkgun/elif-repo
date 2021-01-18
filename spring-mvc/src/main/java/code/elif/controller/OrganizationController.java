788a39178a7533b3fff0de9b52bcbdab9ba293c8
elif.akgun <potrak@gmail.com>
2021-01-18 12:58:40 +0300

    before log implementation (SLF4J: Defaulting to no-operation (NOP) logger implementation)

--- spring-mvc/src/main/java/code/elif/controller/OrganizationController.java
+++ spring-mvc/src/main/java/code/elif/controller/OrganizationController.java
@@ -2,6 +2,8 @@ package code.elif.controller;
 
 import code.elif.model.Organization;
 import code.elif.service.OrganizationService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,6 +16,8 @@ public class OrganizationController {
 
     private OrganizationService organizationService;
 
+    private static Logger logger = LoggerFactory.getLogger("Organization Logger");
+
     @Autowired
     public OrganizationController(OrganizationService organizationService) {
         this.organizationService = organizationService;
@@ -21,13 +25,17 @@ public class OrganizationController {
 
     @RequestMapping("organization-list")
     public String listOrganizations(Model model){
+        logger.info("organization-list controller is called");
         List<Organization> allOrganisations = organizationService.getAllOrganisations();
         model.addAttribute("allOrganisations",allOrganisations);
+        logger.info("organization-list view is returning");
         return "organization-list";
     }
 
     @RequestMapping(value = "jndi-example")
     public String organisationJNDI(){
+        logger.info("jndi-example controller is called");
+        logger.info("jndi-example view is returning");
         return "jndi-example";
     }
 }
