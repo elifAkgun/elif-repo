267169797ba381c4866ca4a4899c1a5ed6bd8e14
elif.akgun <potrak@gmail.com>
2021-05-26 09:50:49 +0300

    web mvc client application

---
+++ WebClient/src/test/java/code/elif/api/ws/WebClient/WebClientApplicationTests.java
@@ -0,0 +1,13 @@
+package code.elif.api.ws.WebClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
