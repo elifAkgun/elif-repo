7188a6c86dac1dbf8c98cecfda64dbb7a80b910c
elif.akgun <potrak@gmail.com>
2021-06-04 21:26:51 +0300

    api config server initial commit

---
+++ SpringBoot/ApiConfigServer/src/main/java/code/elif/ApiConfigServer/ApiConfigServerApplication.java
@@ -0,0 +1,13 @@
+package code.elif.ApiConfigServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiConfigServerApplication.class, args);
+	}
+
+}
