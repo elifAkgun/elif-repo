95bd0fb7e033a864eba056006c22b9e3ec050f4a
elif.akgun <potrak@gmail.com>
2022-01-23 22:38:44 +0300

    added unit tests

--- spring-mvc-cambly/src/test/java/code/elif/controller/CamblyControllerTest.java
+++ spring-mvc-cambly/src/test/java/code/elif/controller/CamblyControllerTest.java
@@ -1,5 +1,108 @@
-import static org.junit.Assert.*;
+package code.elif.controller;
 
+
+import code.elif.model.Cambly;
+import code.elif.service.impl.CamblyServiceImpl;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+
+@ExtendWith(MockitoExtension.class)
 public class CamblyControllerTest {
 
+    @Autowired
+    MockMvc mockMvc;
+
+    @Mock
+    CamblyServiceImpl camblyService;
+
+    @InjectMocks
+    CamblyController controller;
+
+    private Cambly cambly;
+    private List<Cambly> camblies;
+
+    @BeforeEach
+    public void initVariable() {
+        cambly = new Cambly("Helo", "Hello", 1, LocalDate.now());
+        camblies = Arrays.asList(cambly);
+    }
+
+    @Test
+    public void listCambly_checkViewName() throws Exception {
+
+        MockMvc mockMvc = standaloneSetup(controller)
+                .build();
+
+        MvcResult mvcResult = mockMvc.perform(get("/cambly/list")
+                .accept(APPLICATION_JSON)).andReturn();
+
+        Assertions.assertEquals("list-cambly",mvcResult.getModelAndView().getViewName());
+
+    }
+
+    @Test
+    public void listCambly_checkModelAttribute_hasCamblyList() throws Exception {
+        MockMvc mockMvc = standaloneSetup(controller)
+                .build();
+
+        MvcResult mvcResult = mockMvc.perform(get("/cambly/list")
+                .accept(APPLICATION_JSON)).andReturn();
+
+        Assertions.assertEquals(true,mvcResult.getModelAndView().getModel().containsKey("camblyList"));
+    }
+
+
+    @Test
+    public void listCambly_checkModel_camblyList() throws Exception {
+        when(camblyService.getAllCamblys())
+                .thenReturn(camblies);
+
+        MockMvc mockMvc = standaloneSetup(controller)
+                .build();
+
+        MvcResult mvcResult = mockMvc.perform(get("/cambly/list")
+                .accept(APPLICATION_JSON)).andReturn();
+
+        Assertions.assertEquals(camblies,mvcResult.getModelAndView().getModel().get("camblyList"));
+
+    }
+
+    @Test
+    public void addCambly() {
+    }
+
+    @Test
+    public void saveCambly() {
+    }
+
+    @Test
+    public void testSaveCambly() {
+    }
+
+    @Test
+    public void deleteCambly() {
+    }
+
+    @Test
+    public void searchCamblys() {
+    }
 }
--- spring-mvc-cambly/src/test/java/code/elif/service/impl/CamblyServiceImplTest.java
+++ spring-mvc-cambly/src/test/java/code/elif/service/impl/CamblyServiceImplTest.java
@@ -1,5 +1,80 @@
-import static org.junit.Assert.*;
+package code.elif.service.impl;
 
+import code.elif.dao.impl.CamblyDaoImpl;
+import code.elif.model.Cambly;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
 public class CamblyServiceImplTest {
 
+    @Mock
+    CamblyDaoImpl camblyDao;
+
+    @InjectMocks
+    CamblyServiceImpl camblyService;
+
+    private Cambly cambly;
+    private List<Cambly> camblies;
+
+    @BeforeEach
+    public void initVariable() {
+        cambly = new Cambly("Helo", "Hello", 1, LocalDate.now());
+        camblies = Arrays.asList(cambly);
+    }
+
+    @Test
+    public void getAllCamblys() {
+        when(camblyDao.getAllCamblys()).thenReturn(camblies);
+        Assertions.assertEquals(camblies, camblyService.getAllCamblys());
+    }
+
+    @Test
+    public void createCambly() {
+        when(camblyDao.createCambly(cambly)).thenReturn(true);
+        Assertions.assertEquals(true, camblyService.createCambly(cambly));
+    }
+
+    @Test
+    public void getCambly() {
+        when(camblyDao.searchCamblys("Hello")).thenReturn(camblies);
+        Assertions.assertEquals(camblies, camblyService.searchCamblys("Hello"));
+    }
+
+    @Test
+    public void deleteCambly() {
+        camblyService.deleteCambly(1);
+        verify(camblyDao).deleteCambly(1);
+    }
+
+    @Test
+    public void updateCambly() {
+        cambly.setId(1);
+        Cambly updatedCambly = new Cambly("Aple", "Apple", 100, LocalDate.now());
+        updatedCambly.setLessonId(10);
+
+        when(camblyDao.updateCambly(cambly)).thenReturn(updatedCambly);
+
+        Assertions.assertEquals(updatedCambly, camblyService.updateCambly(cambly));
+
+        verify(camblyDao).updateCambly(cambly);
+
+    }
+
+    @Test
+    public void searchCamblys() {
+    }
 }
