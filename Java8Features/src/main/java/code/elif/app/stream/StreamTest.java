c995a19c73b961641b2a35e030cfd3d8cc5b6a2d
elif.akgun <potrak@gmail.com>
2022-03-08 22:20:32 +0300

    collected sorted() method

--- Java8Features/src/main/java/code/elif/app/stream/StreamTest.java
+++ Java8Features/src/main/java/code/elif/app/stream/StreamTest.java
@@ -3,7 +3,6 @@ package code.elif.app.stream;
 import code.elif.app.Shape;
 import code.elif.app.ShapeService;
 
-import javax.swing.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -11,31 +10,47 @@ import java.util.stream.Collectors;
 
 public class StreamTest {
 
-    static ShapeService shapeService = new ShapeService();
-    static List<Shape>  shapeList = (List<Shape>) shapeService.getShapes(new ArrayList());
+    static ShapeService shapeService;
+    static List<Shape> shapeList;
+    static List<Integer> list = Arrays.asList(0, 5, 10, 15, 20, 25);
 
     public static void main(String[] args) {
-      //  runFilterExample();
-      //  runMapExample();
-        processByCount();
+        //  runFilterExample();
+        //  runMapExample();
+        // processByCount();
+        processBySorted();
+    }
+
+    private static void processBySorted() {
+        List<Integer> list1 = list.stream().map(l -> l * 2).sorted().collect(Collectors.toList());
+        System.out.println("ascending order" + list1);
+
+        List<Integer> list2 = list.stream()
+                .map(l -> l * 2).sorted((a, b) -> a > b ? -1 : 1)
+                .collect(Collectors.toList());
+        System.out.println("descending order" + list2);
     }
 
     private static void processByCount() {
-       long count = shapeList.stream()
+        shapeService = new ShapeService();
+        long count = shapeList.stream()
                 .filter(s -> s.getArea() > 50)
                 .count();
         System.out.println("Only " + count + " shape(s) founded in the list.");
     }
 
     private static void runMapExample() {
-        List<Integer> list = Arrays.asList(0,5,10,15,20,25);
-        List<Integer> doubledList = list.stream().map(l-> l*2).collect(Collectors.toList());
-        for(int l : doubledList){
+        shapeService = new ShapeService();
+        shapeList = (List<Shape>) shapeService.getShapes(new ArrayList());
+        List<Integer> doubledList = list.stream().map(l -> l * 2).collect(Collectors.toList());
+        for (int l : doubledList) {
             System.out.println(l);
         }
     }
 
     private static void runFilterExample() {
+        shapeList = (List<Shape>) shapeService.getShapes(new ArrayList());
+
         List<Shape> shapeList = (List<Shape>) shapeService.getShapes(new ArrayList());
 
         List<Shape> collectedList = shapeList.stream()
