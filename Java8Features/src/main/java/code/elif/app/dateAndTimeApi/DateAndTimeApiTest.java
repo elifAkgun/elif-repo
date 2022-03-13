1617b63350e788f25c1c456acc9df55f885e5d8a
elif.akgun <potrak@gmail.com>
2022-03-13 20:25:02 +0300

    added date and time api

---
+++ Java8Features/src/main/java/code/elif/app/dateAndTimeApi/DateAndTimeApiTest.java
@@ -0,0 +1,19 @@
+package code.elif.app.dateAndTimeApi;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+public class DateAndTimeApiTest {
+
+    public static void main(String[] args) {
+        LocalDate date = LocalDate.now();
+        System.out.println(date);
+
+        LocalTime time = LocalTime.now();
+        System.out.println(time);
+
+        LocalDateTime dateTime = LocalDateTime.now();
+        System.out.println(dateTime);
+    }
+}
