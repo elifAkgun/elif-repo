9d46713b0f3f68afd98fe82558e13b529c097f2d
elif.akgun <potrak@gmail.com>
2022-03-16 22:33:49 +0300

    added period class sample

--- Java8Features/src/main/java/code/elif/app/dateAndTimeApi/PeriodTest.java
+++ Java8Features/src/main/java/code/elif/app/dateAndTimeApi/PeriodTest.java
@@ -1,4 +1,18 @@
 package code.elif.app.dateAndTimeApi;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Period;
+
 public class PeriodTest {
+
+    public static void main(String[] args) {
+        LocalDate birthDate = LocalDate.of(1989, 9, 04);
+        LocalDate now = LocalDate.now();
+
+        Period period = Period.between(birthDate, now);
+
+        System.out.printf("There are %d years, %d months, %d days between your age and now."
+                , period.getYears(), period.getMonths(), period.getDays());
+    }
 }
