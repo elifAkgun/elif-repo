5725e7fb25d7212ec1f2c146fb7ca626052ab120
elif.akgun <potrak@gmail.com>
2022-11-09 23:23:33 +0300

    deleted refactored folder and unnecessary files

--- bootMySqlDocker/src/main/java/code/elif/bootMySqlDocker/repository/CamblyRepository.java
+++
@@ -1,6 +0,0 @@
-package code.elif.bootMySqlDocker.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CamblyRepository extends JpaRepository<CamblyDTO, Long> {
-}
