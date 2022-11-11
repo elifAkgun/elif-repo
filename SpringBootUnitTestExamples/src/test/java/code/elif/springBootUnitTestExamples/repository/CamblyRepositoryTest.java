3f8b829649fdd780ff37ef1623cfab861231f82d
elif.akgun <potrak@gmail.com>
2022-11-11 16:41:47 +0300

    refactored tests

--- SpringBootUnitTestExamples/src/test/java/code/elif/springBootUnitTestExamples/repository/CamblyRepositoryTest.java
+++ SpringBootUnitTestExamples/src/test/java/code/elif/springBootUnitTestExamples/repository/CamblyRepositoryTest.java
@@ -3,8 +3,6 @@ package code.elif.springBootUnitTestExamples.repository;
 import code.elif.springBootUnitTestExamples.repository.model.Cambly;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -17,7 +15,6 @@ import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@ExtendWith(MockitoExtension.class)
 @DataJpaTest
 public class CamblyRepositoryTest {
 
--- SpringBootUnitTestExamples/src/test/java/code/elif/springBootUnitTestExamples/repository/integration/CamblyRepositoryIntegrationTest.java
+++ SpringBootUnitTestExamples/src/test/java/code/elif/springBootUnitTestExamples/repository/integration/CamblyRepositoryIntegrationTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.persistence.EntityManager;
@@ -19,6 +20,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@PropertySource(value ="classpath:application-integration.properties")
 public class CamblyRepositoryIntegrationTest {
 
     public static final String MISTAKE = "MISTAKE";
