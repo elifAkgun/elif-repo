c81dfaa95c40639a484ba3ab9dec6a32954499bd
elif.akgun <potrak@gmail.com>
2023-02-15 18:02:49 +0300

    added time out example

--- ReactiveProgramming/src/main/java/code/elif/operators/TimeOutExample.java
+++ ReactiveProgramming/src/main/java/code/elif/operators/TimeOutExample.java
@@ -1,4 +1,34 @@
 package code.elif.operators;
 
+import code.elif.ReactiveStreamUtil;
+import com.github.javafaker.Faker;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+
+import static java.time.Duration.ofMillis;
+import static java.time.Duration.ofSeconds;
+
 public class TimeOutExample {
+
+    public static void main(String[] args) throws InterruptedException {
+        getAnimalName()
+                .log()
+                .timeout(ofSeconds(2), fallbackAnimal())
+                .subscribe(ReactiveStreamUtil.getConsumer());
+
+        Thread.sleep(Duration.ofSeconds(5).toMillis());
+    }
+
+    private static Flux<Object> getAnimalName() {
+        return Flux.generate(synchronousSink -> {
+            synchronousSink.next(Faker.instance().animal().name());
+        }).delayElements(ofSeconds(5));
+    }
+
+    private static Flux<Object> fallbackAnimal() {
+        return Flux.generate(synchronousSink -> {
+            synchronousSink.next(Faker.instance().animal().name());
+        }).delayElements(ofMillis(200));
+    }
 }
