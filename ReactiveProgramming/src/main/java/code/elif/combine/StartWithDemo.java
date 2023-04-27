c41080b42daa2c69fa4036e5c9ee42807bcd8d27
elif.akgun <potrak@gmail.com>
2023-04-27 15:27:42 +0300

    renamed class name

--- ReactiveProgramming/src/main/java/code/elif/combine/StartWithDemo.java
+++ ReactiveProgramming/src/main/java/code/elif/combine/StartWithDemo.java
@@ -2,7 +2,7 @@ package code.elif.combine;
 
 import code.elif.ReactiveStreamUtil;
 
-public class StartWith {
+public class StartWithDemo {
 
     public static void main(String[] args) {
         NameGenerator nameGenerator = new NameGenerator();
