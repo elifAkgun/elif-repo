6825478ca52268a32823449097a7ffdf8861ea93
elif.akgun <potrak@gmail.com>
2023-05-09 20:40:03 +0300

    added validation for routerconfig

--- webfluxdemo/src/main/java/code/elif/webfluxdemo/controller/config/RouterConfig.java
+++ webfluxdemo/src/main/java/code/elif/webfluxdemo/controller/config/RouterConfig.java
@@ -5,10 +5,7 @@ import code.elif.webfluxdemo.exception.InputValidationException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.function.server.RouterFunction;
-import org.springframework.web.reactive.function.server.RouterFunctions;
-import org.springframework.web.reactive.function.server.ServerRequest;
-import org.springframework.web.reactive.function.server.ServerResponse;
+import org.springframework.web.reactive.function.server.*;
 import reactor.core.publisher.Mono;
 
 import java.util.function.BiFunction;
@@ -21,6 +18,7 @@ public class RouterConfig {
 
     /**
      * May be more than one router function in your project.
+     *
      * @return
      */
     @Bean
@@ -32,15 +30,19 @@ public class RouterConfig {
 
     private RouterFunction<ServerResponse> serverResponseRouterFunction() {
         return RouterFunctions.route()
-                .GET("square/{input}", requestHandler::square)
+                .GET("square/{input}", RequestPredicates.path("/[0-9]|1[0-9]|20"), requestHandler::square)
+                .GET("square/{input}", request ->
+                        ServerResponse.badRequest()
+                                .bodyValue("Number only should be between 1-20"))
                 .GET("multiplicationTable/{input}", requestHandler::getMultiplicationTable)
                 .POST("multiplication", requestHandler::getMultiplication)
-                .onError(InputValidationException.class,exceptionHandler())
+                .onError(InputValidationException.class, exceptionHandler())
                 .build();
     }
 
     /**
      * May be more than one router function in your project.
+     *
      * @return
      */
     @Bean
@@ -49,7 +51,7 @@ public class RouterConfig {
                 .GET("router/square1/{input1}", requestHandler::square)
                 .GET("router/multiplicationTable1/{input}", requestHandler::getMultiplicationTable)
                 .POST("router/multiplication1", requestHandler::getMultiplication)
-                .onError(InputValidationException.class,exceptionHandler())
+                .onError(InputValidationException.class, exceptionHandler())
                 .build();
     }
 
