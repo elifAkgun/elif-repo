b63b1f12fe1621347bacd3cf7995fb59ca67d4af
elif.akgun <potrak@gmail.com>
2023-07-23 12:09:26 +0300

    fixed CatalogServiceApplicationTests

--- Cloud Native Spring In Action/CatalogService/src/test/java/code/elif/catalog/CatalogServiceApplicationTests.java
+++ Cloud Native Spring In Action/CatalogService/src/test/java/code/elif/catalog/CatalogServiceApplicationTests.java
@@ -3,7 +3,7 @@ package code.elif.catalog;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = CatalogServiceApplicationTests.class)
 class CatalogServiceApplicationTests {
 
 	@Test
