686c1ad7d73c7bab2fea18cdb4a04a73a4faf082
elif.akgun <potrak@gmail.com>
2025-04-07 22:50:19 +0300

    added PinningThraadApp example and changed jdk version

---
+++ Java Virtual Thread/src/main/java/code/elif/thread/virtual/synchronization/PinningThraadApp.java
@@ -0,0 +1,40 @@
+package code.elif.thread.virtual.synchronization;
+
+import code.elif.util.CommonUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+
+
+public class PinningThraadApp {
+
+    private static final Logger logger = LoggerFactory.getLogger(PinningThraadApp.class);
+
+    static {
+        System.setProperty("jdk.tracePinnedThreads", "full");
+    }
+
+    public static void main(String[] args) {
+
+        Runnable runnable = () -> logger.info("Pinned Thread is blocking me!!");
+        demo(Thread.ofVirtual());
+        Thread.ofVirtual().start(runnable);
+
+        CommonUtils.sleep(Duration.ofSeconds(20));
+    }
+
+    public static void demo(Thread.Builder threadBuilder) {
+        for (int i = 0; i < 100; i++) {
+            threadBuilder.start(() -> {
+                logger.info("IO tasks are started...");
+                ioTask();
+                logger.info("IO tasks are finished...");
+            });
+        }
+    }
+
+    private static synchronized void ioTask() {
+        CommonUtils.sleep(Duration.ofSeconds(1000));
+    }
+}
