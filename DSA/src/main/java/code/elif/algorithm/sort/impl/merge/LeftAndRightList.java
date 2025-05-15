062a91497a9879cf625b2c88d9e2be0783f19202
elif.akgun <potrak@gmail.com>
2025-05-16 00:01:33 +0300

    reformated codes

--- DSA/src/main/java/code/elif/algorithm/sort/impl/merge/LeftAndRightList.java
+++ DSA/src/main/java/code/elif/algorithm/sort/impl/merge/LeftAndRightList.java
@@ -3,5 +3,5 @@ package code.elif.algorithm.sort.impl.merge;
 import java.util.List;
 
 public record LeftAndRightList<T>(
-        List<T> right, List<T>left){
-        }
+        List<T> right, List<T> left) {
+}
